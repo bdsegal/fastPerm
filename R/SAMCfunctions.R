86f7075fe05654e5173f018751ac03232812ea53
bdsegal <bdsegal@umich.edu>
2016-02-02 14:58:08 -0500

    Remove tabs, Revise Readme

--- R/SAMCfunctions.R
+++ R/SAMCfunctions.R
@@ -1,41 +1,41 @@
 # Implementation of SAMC algorithm 
 
 swapFun <- function(x, y, L){
-	#' Utility function for SAMC function
-	#'
-	#' This function is used by SAMC to produce the next random permutation
-	#' @param x First sample
-	#' @param y Second sample
-	#' @param L Number of elements to swap
-	#' @keywords SAMC utility
-	#' @export
-	#' @examples
-	#' swapFun(x, y, L)
-	
-	
-	nx <- length(x)
-	ny <- length(y)
-	
-	starX <- sample(1:nx, L)
-	starY <- sample(1:ny, L)
-	
-	xstar <- c(x[-starX], y[starY])
-	ystar <- c(y[-starY], x[starX])
-	
-	return(list(xstar = xstar, ystar = ystar))
+  #' Utility function for SAMC function
+  #'
+  #' This function is used by SAMC to produce the next random permutation
+  #' @param x First sample
+  #' @param y Second sample
+  #' @param L Number of elements to swap
+  #' @keywords SAMC utility
+  #' @export
+  #' @examples
+  #' swapFun(x, y, L)
+
+  nx <- length(x)
+  ny <- length(y)
+  
+  starX <- sample(1:nx, L)
+  starY <- sample(1:ny, L)
+  
+  xstar <- c(x[-starX], y[starY])
+  ystar <- c(y[-starY], x[starX])
+  
+  return(list(xstar = xstar, ystar = ystar))
 }
 
 gammaFun <- function(b, b0){
-	#' Utility function for SAMC function
-	#'
-	#' This function is used by SAMC to deteroriate the influence of updates
-	#' @param b Current iteration of the SAMC algorithim
-	#' @param bo Iteration at which the updates begin to deteriorate.
-	#' @keywords SAMC utility
-	#' @export
-	#' @examples
-	#' gammaFun(b, b0)
-	b0 / max(b, b0)
+  #' Utility function for SAMC function
+  #'
+  #' This function is used by SAMC to deteroriate the influence of updates
+  #' @param b Current iteration of the SAMC algorithim
+  #' @param bo Iteration at which the updates begin to deteriorate.
+  #' @keywords SAMC utility
+  #' @export
+  #' @examples
+  #' gammaFun(b, b0)
+  
+  b0 / max(b, b0)
 }
 
 SAMC <- function(x, y, testStat=ratioMean, B=10e4, m=300, b0=1000){
@@ -65,153 +65,148 @@ SAMC <- function(x, y, testStat=ratioMean, B=10e4, m=300, b0=1000){
 #' sam
 #' print(sam)
 
-	if (attributes(testStat)$comparison == "ratio" & 
-			(min(x,y)<0)) {
-		stop("Some sample values <=0. With ratio statistics, all values must be greater than 0.")
-	}
-	
-	### keep track of algorithm
-	numAccepts <- 0
-	piObs <- rep(0, m)
-	
-	###########################
-	# initialize quantitiess
-	n <- min(length(x), length(y))
-	
-	t0 <- testStat(x, y)
+  if (attributes(testStat)$comparison == "ratio" & (min(x,y)<0)) {
+    stop("Some sample values <=0. With ratio statistics, all values must be greater than 0.")
+  }
+  
+  ### keep track of algorithm
+  numAccepts <- 0
+  piObs <- rep(0, m)
+  
+  ###########################
+  # initialize quantitiess
+  n <- min(length(x), length(y))
+  
+  t0 <- testStat(x, y)
 
-	if (attributes(testStat)$comparison == "difference"){
-		lambda <- seq(0, t0, length.out=m)
-	} else {
-		lambda <- seq(1, t0, length.out=m)
-	}
-	
-	theta <- rep(0, m)
-	piVec <- rep(1 / m, m)
+  if (attributes(testStat)$comparison == "difference"){
+    lambda <- seq(0, t0, length.out=m)
+  } else {
+    lambda <- seq(1, t0, length.out=m)
+  }
+  
+  theta <- rep(0, m)
+  piVec <- rep(1 / m, m)
 
-	L <- ceiling(n * 0.075)
-	
-	# get initial test stat and partition
-	tInit <- testStat(x, y)
-	jInit <- sum(tInit >= lambda)
-	
-	for (b in 1:B){
-		
-		# propose update
-		swapOut <- swapFun(x, y, L)
-		xNew <- swapOut$xstar
-		yNew <- swapOut$ystar
+  L <- ceiling(n * 0.075)
+  
+  # get initial test stat and partition
+  tInit <- testStat(x, y)
+  jInit <- sum(tInit >= lambda)
+  
+  for (b in 1:B){
+    # propose update
+    swapOut <- swapFun(x, y, L)
+    xNew <- swapOut$xstar
+    yNew <- swapOut$ystar
 
-		# get new test stat and partition
-		# T_new <- abs(t.test(xNew,yNew)$statistic)
-		tNew <- testStat(xNew, yNew)
-		jNew <- sum(tNew >= lambda)
-		
-		# get acceptance probability and decide whether to accept
-		r = min(1, exp(theta[jInit] - theta[jNew]))
-		accept <- (runif(n = 1) <= r)
-			
-		# set new (x,y) and update weights
-		if (accept){
-		
-			x <- xNew
-			y <- yNew
-			
-			I <- rep(0, m)
-			I[jNew] <- 1
-			theta <- theta + gammaFun(b, b0) * (I-piVec)
-			
-			tInit <- tNew
-			jInit <- jNew
-			
-			numAccepts <- numAccepts + 1
-			
-		} else{
-		
-			I <- rep(0, m)
-			I[jInit] <- 1
-			theta <- theta + gammaFun(b, b0) * (I-piVec)	
-			
-		}
+    # get new test stat and partition
+    # T_new <- abs(t.test(xNew,yNew)$statistic)
+    tNew <- testStat(xNew, yNew)
+    jNew <- sum(tNew >= lambda)
+    
+    # get acceptance probability and decide whether to accept
+    r = min(1, exp(theta[jInit] - theta[jNew]))
+    accept <- (runif(n = 1) <= r)
+      
+    # set new (x,y) and update weights
+    if (accept){
+      x <- xNew
+      y <- yNew
+      
+      I <- rep(0, m)
+      I[jNew] <- 1
+      theta <- theta + gammaFun(b, b0) * (I-piVec)
+      
+      tInit <- tNew
+      jInit <- jNew
+      
+      numAccepts <- numAccepts + 1
+      
+    } else{
+      I <- rep(0, m)
+      I[jInit] <- 1
+      theta <- theta + gammaFun(b, b0) * (I-piVec)
+    }
 
-		piObs <- piObs + I
-		
-	}
+    piObs <- piObs + I
+  }
 
-	# estimate p-value
-	pval = exp(theta[m]) * piVec[m] / sum(exp(theta) * piVec)
-	
-	ret <- list(pval = pval,
-				numAccepts = numAccepts,
-				piObs = piObs,
-				theta = theta,
-				B = B,
-				t0 = t0,
-				comparison = attributes(testStat)$comparison,
-				summary = attributes(testStat)$summary)
-	class(ret) <- "SAMC"
-	
-	return(ret)
+  # estimate p-value
+  pval = exp(theta[m]) * piVec[m] / sum(exp(theta) * piVec)
+  
+  ret <- list(pval = pval,
+        numAccepts = numAccepts,
+        piObs = piObs,
+        theta = theta,
+        B = B,
+        t0 = t0,
+        comparison = attributes(testStat)$comparison,
+        summary = attributes(testStat)$summary)
+
+  class(ret) <- "SAMC"
+  
+  return(ret)
 }
 
 print.SAMC <- function(sam){
-	#' Print function for SAMC
-	#'
-	#' This function prints the reslts and convergence diagnostics for the SAMC algorithm.
-	#' If the SAMC converged, then it should have sampled nearly uniformly from all regions. The maximum discrepancy is the maximum difference between the number of times SAMC sampled from a region and the expected amount if the sampling were uniform. Yu et al. suggest that the maximum discrepancy should be less than 0.2.
-	#' @param sam Output from the SAMC function
-	#' @keywords SAMC summary convergence diagnostics
-	#' @export
-	#' @examples
-	#' #' x <- rexp(100, 4)
-	#' y <- rexp(100, 2)
-	#' sam <- SAMC(x, y)
-	#' print(sam)
-	
-	x <- sam$piObs
-	m0 <- sum(x==0)
-	p <- x/sum(x)
-	m <- length(p)
+  #' Print function for SAMC
+  #'
+  #' This function prints the reslts and convergence diagnostics for the SAMC algorithm.
+  #' If the SAMC converged, then it should have sampled nearly uniformly from all regions. The maximum discrepancy is the maximum difference between the number of times SAMC sampled from a region and the expected amount if the sampling were uniform. Yu et al. suggest that the maximum discrepancy should be less than 0.2.
+  #' @param sam Output from the SAMC function
+  #' @keywords SAMC summary convergence diagnostics
+  #' @export
+  #' @examples
+  #' #' x <- rexp(100, 4)
+  #' y <- rexp(100, 2)
+  #' sam <- SAMC(x, y)
+  #' print(sam)
+  
+  x <- sam$piObs
+  m0 <- sum(x==0)
+  p <- x/sum(x)
+  m <- length(p)
 
-	error <- (p-1/(m-m0))/(1/(m-m0))
-	error <- error*(x>0)
-	
-	result <- paste("    SAMC Two Sample Test    \n\n",
-		sam$comparison, " of ", sam$summary, "s\n",
-		prettyNum(sam$B, big.mark=","), " total iterations",
-		"\n\nobserved statistic = ", signif(sam$t0,3),
-		"\np-value = ", signif(sam$pval,3),
-		"\n\nmax discrepancy = ", signif(max(abs(error)),2),"\nalgorithm has converged if max discrepancy < 0.2", sep = "")
-	
-	writeLines(result)
+  error <- (p-1/(m-m0))/(1/(m-m0))
+  error <- error*(x>0)
+  
+  result <- paste("    SAMC Two Sample Test    \n\n",
+    sam$comparison, " of ", sam$summary, "s\n",
+    prettyNum(sam$B, big.mark=","), " total iterations",
+    "\n\nobserved statistic = ", signif(sam$t0,3),
+    "\np-value = ", signif(sam$pval,3),
+    "\n\nmax discrepancy = ", signif(max(abs(error)),2),"\nalgorithm has converged if max discrepancy < 0.2", sep = "")
+  
+  writeLines(result)
 }
 
 plot.SAMC <- function(sam){
-	#' plot function for SAMC
-	#'
-	#' This function plots the convergence diagnostics for the SAMC algorithm and prints out the associated values
-	#' If the SAMC converged, then it should have sampled nearly uniformly from all regions. The maximum discrepancy is the maximum difference between the number of times SAMC sampled from a region and the expected amount if the sampling were uniform. Yu et al. suggest that the maximum discrepancy should be less than 0.2.
-	#' @param sam Output from the SAMC function
-	#' @keywords SAMC summary convergence diagnostics
-	#' @export
-	#' @examples
-	#' #' x <- rexp(100, 4)
-	#' y <- rexp(100, 2)
-	#' sam <- SAMC(x, y)
-	#' plot(sam)
-	
-	x <- sam$piObs
-	m0 <- sum(x==0)
-	p <- x/sum(x)
-	m <- length(p)
+  #' plot function for SAMC
+  #'
+  #' This function plots the convergence diagnostics for the SAMC algorithm and prints out the associated values
+  #' If the SAMC converged, then it should have sampled nearly uniformly from all regions. The maximum discrepancy is the maximum difference between the number of times SAMC sampled from a region and the expected amount if the sampling were uniform. Yu et al. suggest that the maximum discrepancy should be less than 0.2.
+  #' @param sam Output from the SAMC function
+  #' @keywords SAMC summary convergence diagnostics
+  #' @export
+  #' @examples
+  #' #' x <- rexp(100, 4)
+  #' y <- rexp(100, 2)
+  #' sam <- SAMC(x, y)
+  #' plot(sam)
+  
+  x <- sam$piObs
+  m0 <- sum(x==0)
+  p <- x/sum(x)
+  m <- length(p)
 
-	error <- (p-1/(m-m0))/(1/(m-m0))
-	error <- error*(x>0)
-	
-	maxDiscrep <- signif(max(abs(error)),2)
-	
+  error <- (p-1/(m-m0))/(1/(m-m0))
+  error <- error*(x>0)
+  
+  maxDiscrep <- signif(max(abs(error)),2)
+  
   barplot(x, xlab="SAMC region", ylab="count", 
-		main=paste("SAMC sampling distribution (uniform at convergence)",
-		"\nmax discrepancy = ", maxDiscrep, " (<0.2 at convergence)",
-		sep = ""))
+    main=paste("SAMC sampling distribution (uniform at convergence)",
+    "\nmax discrepancy = ", maxDiscrep, " (<0.2 at convergence)",
+    sep = ""))
 }
--- R/fastPermFunctions.R
+++ R/fastPermFunctions.R
@@ -3,61 +3,62 @@
 
 # pmf of partitions ---------------------------------------
 Pi=function(nx,ny){
-	#' Exact pmf of partitions
-	#'
-	#' This function calculates the exact probability mass function
-	#' of the partitions.
-	#' Typically, this function is called from fastPerm.
-	#' @param nx Size of first sample
-	#' @param ny Size of second sample
-	#' @keywords partition pmf
-	#' @export
-	#' @examples
-	#' Pi(nx, ny)
-	
-	out <- rep(NA, min(nx, ny) + 1)
-	names(out) <- 0:min(nx, ny)
-	for (m in 0:min(nx, ny)){
-		out[m + 1] <- choose(nx, m) * choose(ny, m) / choose(nx + ny, nx)
-	}
-	
-	return(out)
+  #' Exact pmf of partitions
+  #'
+  #' This function calculates the exact probability mass function
+  #' of the partitions.
+  #' Typically, this function is called from fastPerm.
+  #' @param nx Size of first sample
+  #' @param ny Size of second sample
+  #' @keywords partition pmf
+  #' @export
+  #' @examples
+  #' Pi(nx, ny)
+  
+  out <- rep(NA, min(nx, ny) + 1)
+  names(out) <- 0:min(nx, ny)
+  
+  for (m in 0:min(nx, ny)){
+    out[m + 1] <- choose(nx, m) * choose(ny, m) / choose(nx + ny, nx)
+  }
+  
+  return(out)
 }
 
 stir <- function(n){
-	#' Utility function for PiStirling
-	#'
-	#' This function is used by the stirling function
-	#' @param n Integer 
-	#' @keywords stirling utility
-	#' @export
-	#' @examples
-	#' stir(n)
+  #' Utility function for PiStirling
+  #'
+  #' This function is used by the stirling function
+  #' @param n Integer 
+  #' @keywords stirling utility
+  #' @export
+  #' @examples
+  #' stir(n)
 
-	ifelse(n > 0, n * log(n), 0)
+  ifelse(n > 0, n * log(n), 0)
 }
 
 stirling <- function(nx, ny, m){
-	#' Utility function for PiStirling
-	#'
-	#' This function is used by the PiStirling function to
-	#' calculate the mass in partition m
-	#' @param nx Size of first sample 
-	#' @param nx Size of second sample 
-	#' @param m Partition
-	#' @keywords stirling utility
-	#' @export
-	#' @examples
-	#' stirling(nx, ny, m)
+  #' Utility function for PiStirling
+  #'
+  #' This function is used by the PiStirling function to
+  #' calculate the mass in partition m
+  #' @param nx Size of first sample 
+  #' @param nx Size of second sample 
+  #' @param m Partition
+  #' @keywords stirling utility
+  #' @export
+  #' @examples
+  #' stirling(nx, ny, m)
 
-	exp(2*stir(nx) + 2*stir(ny) - stir(nx-m) -stir(ny-m) - stir(nx+ny) - 2*stir(m))
+  exp(2*stir(nx) + 2*stir(ny) - stir(nx-m) -stir(ny-m) - stir(nx+ny) - 2*stir(m))
 }
 
 PiStirling <- function(nx, ny){
 #' Stirling approximation of the pmf of partitions
 #'
 #' This function calculates the Stirling approximation to the
-#	 probability mass function of the partitions.
+#  probability mass function of the partitions.
 #' Typically, this function is called from fastPerm.
 #' @param nx Size of first sample
 #' @param ny Size of second sample
@@ -66,79 +67,79 @@ PiStirling <- function(nx, ny){
 #' @examples
 #' PiStirling(nx, ny)
 
-	out <- rep(NA, min(nx, ny) + 1)
-	names(out) <- 0:min(nx, ny)
-	
-	for (m in 0:min(nx, ny)){
-		out[m + 1] <- stirling(nx, ny, m)
-	}
-	
-	return(out / sum(out))
+  out <- rep(NA, min(nx, ny) + 1)
+  names(out) <- 0:min(nx, ny)
+  
+  for (m in 0:min(nx, ny)){
+    out[m + 1] <- stirling(nx, ny, m)
+  }
+  
+  return(out / sum(out))
 }
 
 # test statistics, used as input to fastPerm ---------------
 
 diffMean <- function(x, y){
-	#' Utility function for fastPerm and SAMC
-	#'
-	#' Difference in means test statistic used by fastPerm
-	#' @param x First sample 
-	#' @param y Second sample
-	#' @keywords test statistic
-	#' @export
-	#' @examples
-	#' diffMean(x, y )
-	abs(mean(x) - mean(y))
+  #' Utility function for fastPerm and SAMC
+  #'
+  #' Difference in means test statistic used by fastPerm
+  #' @param x First sample 
+  #' @param y Second sample
+  #' @keywords test statistic
+  #' @export
+  #' @examples
+  #' diffMean(x, y )
+  
+  abs(mean(x) - mean(y))
 }
-attributes(diffMean) <- list(summary="mean",
-														comparison="difference")
+attributes(diffMean) <- list(summary="mean", comparison="difference")
 
 diffMedian <- function(x, y){
-	#' Utility function for fastPerm and SAMC
-	#'
-	#' Difference in medians test statistic used by fastPerm.
-	#' Note: In this version of fastPerm, median statistic may not be reliable.
-	#' @param x First sample 
-	#' @param y Second sample
-	#' @keywords test statistic
-	#' @export
-	#' @examples
-	#' diffMedian(x, y )
-	abs(median(x) - median(y))
+  #' Utility function for fastPerm and SAMC
+  #'
+  #' Difference in medians test statistic used by fastPerm.
+  #' Note: In this version of fastPerm, median statistic may not be reliable.
+  #' @param x First sample 
+  #' @param y Second sample
+  #' @keywords test statistic
+  #' @export
+  #' @examples
+  #' diffMedian(x, y )
+  
+  abs(median(x) - median(y))
 }
-attributes(diffMedian) <- list(summary="median",
-														comparison="difference")
+attributes(diffMedian) <- list(summary="median", comparison="difference")
 
 ratioMean <- function(x, y){
-	#' Utility function for fastPerm and SAMC
-	#'
-	#' Ratio of means test statistic used by fastPerm
-	#' @param x First sample 
-	#' @param y Second sample
-	#' @keywords test statistic
-	#' @export
-	#' @examples
-	#' ratioMean(x, y )
-	max(mean(x) / mean(y), mean(y) / mean(x))
+  #' Utility function for fastPerm and SAMC
+  #'
+  #' Ratio of means test statistic used by fastPerm
+  #' @param x First sample 
+  #' @param y Second sample
+  #' @keywords test statistic
+  #' @export
+  #' @examples
+  #' ratioMean(x, y )
+  
+  max(mean(x) / mean(y), mean(y) / mean(x))
 }
-attributes(ratioMean) <- list(summary="mean",
-														comparison="ratio")
+attributes(ratioMean) <- list(summary="mean", comparison="ratio")
 
 ratioMedian <- function(x, y){
-	#' Utility function for fastPerm and SAMC
-	#'
-	#' Ratio of medians test statistic used by fastPerm.
-	#' Note: In this version of fastPerm, median statistic may not be reliable.
-	#' @param x First sample 
-	#' @param y Second sample
-	#' @keywords test statistic
-	#' @export
-	#' @examples
-	#' ratioMedian(x, y )
-	max(median(x) / median(y), median(y) / median(x))
+  #' Utility function for fastPerm and SAMC
+  #'
+  #' Ratio of medians test statistic used by fastPerm.
+  #' Note: In this version of fastPerm, median statistic may not be reliable.
+  #' @param x First sample 
+  #' @param y Second sample
+  #' @keywords test statistic
+  #' @export
+  #' @examples
+  #' ratioMedian(x, y )
+  
+  max(median(x) / median(y), median(y) / median(x))
 }
-attributes(ratioMedian) <- list(summary="median",
-														comparison="ratio")
+attributes(ratioMedian) <- list(summary="median", comparison="ratio")
 
 # proposed algorithm --------------------------------------
 
@@ -168,158 +169,146 @@ fastPerm <- function(x, y, testStat = ratioMean, B=1000, adjusted=FALSE){#, prin
 #' mStopDiffMean(x, y)
 #' fastPerm(x, y, testStat = diffMean)
 
-	if (attributes(testStat)$comparison == "ratio" & 
-			(min(x,y)<0)) {
-		stop("Some sample values <=0. With ratio statistics, all values must be greater than 0.")
-	}
-	
-	# if (printMstop) {
-		# if (attributes(testStat)$comparison == "ratio") {
-			# mStop <- mStopRatio(x, y, B, plot = plotMstop)
-		# } else if (attributes(testStat)$comparison == "difference") {
-			# mStop <- mStopDiff(x, y, B, plot = plotMstop)
-		# }
-		
-	# writeLines(paste("Expected mStop = ", mStop, "\n\n", sep = ""))
-	
-	# }
-	
-	nx <- length(x)
-	ny <- length(y)
-	
-	# need nx <= ny. swap input if not correctly ordered
-	if (ny<nx){
-		xtemp <- x
-		x <- y
-		y <- xtemp
-		
-		nxtemp <- nx
-		nx <- ny
-		ny <- nxtemp
-	}
-	
-	# try the exact formula first
-	usingStirling <- FALSE # flag for whether the Stirling approx used
+  if (attributes(testStat)$comparison == "ratio" & (min(x,y)<0)) {
+    stop("Some sample values <=0. With ratio statistics, all values must be greater than 0.")
+  }
+  
+  nx <- length(x)
+  ny <- length(y)
+  
+  # need nx <= ny. swap input if not correctly ordered
+  if (ny<nx){
+    xtemp <- x
+    x <- y
+    y <- xtemp
+    
+    nxtemp <- nx
+    nx <- ny
+    ny <- nxtemp
+  }
+  
+  # try the exact formula first
+  usingStirling <- FALSE # flag for whether the Stirling approx used
 
-	pmf <- Pi(nx, ny)
-	
-	# if exact formula fails, use Stirling approximation
-	if (sum(is.nan(pmf)) > 0) {
-		pmf <- PiStirling(nx, ny)
-		usingStirling <- TRUE
-	}
-	
-	mMax <- as.numeric(names(pmf)[which(pmf == max(pmf))])
-	t0 <- testStat(x, y)
-	
-	countTemp <-  rep(0, nx)
-	names(countTemp) <- 1:nx
-	m <- 1
-	sumGTzero <- TRUE
-	
-	while (sumGTzero & (m <= mMax[1])) {
+  pmf <- Pi(nx, ny)
+  
+  # if exact formula fails, use Stirling approximation
+  if (sum(is.nan(pmf)) > 0) {
+    pmf <- PiStirling(nx, ny)
+    usingStirling <- TRUE
+  }
+  
+  mMax <- as.numeric(names(pmf)[which(pmf == max(pmf))])
+  t0 <- testStat(x, y)
+  
+  countTemp <-  rep(0, nx)
+  names(countTemp) <- 1:nx
+  m <- 1
+  sumGTzero <- TRUE
+  
+  while (sumGTzero & (m <= mMax[1])) {
 
-		tb <- rep(NA, B)
-		
-		for (b in 1:B){
-		
-			starX <- sample(1:nx, size = m, replace = FALSE)
-			starY <- sample(1:ny, size = m, replace = FALSE)
+    tb <- rep(NA, B)
+    
+    for (b in 1:B){
+      starX <- sample(1:nx, size = m, replace = FALSE)
+      starY <- sample(1:ny, size = m, replace = FALSE)
 
-			xNew <- c(x[-starX], y[starY])
-			yNew <- c(y[-starY], x[starX])
-			
-			tb[b] <-  testStat(xNew, yNew)
-		}
-		
-		countTemp[m] <- sum(tb >= t0)
-		
-		if (countTemp[m] == 0){
-			sumGTzero <- FALSE
-		} else{
-			m <- m+1
-		}
-	}
-		
-	# setup neighborhoods for prediction, to be symmetric about mMax
-	if (length(mMax)==1 & mMax[1]==nx){
-		mPred <- 1:mMax
-	} else if (length(mMax)==1 & mMax[1]<nx){
-		mPred <- c(1:mMax,((mMax-1):0)[1:(nx-mMax)])
-	} else if (length(mMax)==2 & mMax[2]==nx){
-		mPred <- c(1:mMax[1], mMax[1])
-	} else {
-		mPred <- c(1:mMax[1], (mMax[1]:0)[1:(nx - mMax[1])])
-	}
-	
-	# data frame for regression
-	count <- c(B, countTemp[1:(m-1)]) + 1*adjusted
-	countForReg <- data.frame(count = count, mReg = 0:(m-1))
-	
-	poisFit <- glm(count ~ mReg, family = poisson, data = countForReg)
-	
-	# new data for predictions in partitions m = mPred
-	mNewData <- data.frame(mReg = mPred)
-	
-	# if nx < ny, set p-value in 0 sub-orbit to 1 and 
-	# estimate p-value in sub-orbit nx
-	if (nx < ny) { 
-			
-		# note: predictions with type="response" are not reliable for small values. There is not a problem for type="link"
-		pPoisCount <- c(B + 1*adjusted,
-		  exp(predict(poisFit, newdata=mNewData, type="link")))
+      xNew <- c(x[-starX], y[starY])
+      yNew <- c(y[-starY], x[starX])
+      
+      tb[b] <-  testStat(xNew, yNew)
+    }
+    
+    countTemp[m] <- sum(tb >= t0)
+    
+    if (countTemp[m] == 0){
+      sumGTzero <- FALSE
+    } else{
+      m <- m+1
+    }
+  }
+    
+  # setup neighborhoods for prediction, to be symmetric about mMax
+  if (length(mMax)==1 & mMax[1]==nx){
+    mPred <- 1:mMax
+  } else if (length(mMax)==1 & mMax[1]<nx){
+    mPred <- c(1:mMax,((mMax-1):0)[1:(nx-mMax)])
+  } else if (length(mMax)==2 & mMax[2]==nx){
+    mPred <- c(1:mMax[1], mMax[1])
+  } else {
+    mPred <- c(1:mMax[1], (mMax[1]:0)[1:(nx - mMax[1])])
+  }
+  
+  # data frame for regression
+  count <- c(B, countTemp[1:(m-1)]) + 1*adjusted
+  countForReg <- data.frame(count = count, mReg = 0:(m-1))
+  
+  poisFit <- glm(count ~ mReg, family = poisson, data = countForReg)
+  
+  # new data for predictions in partitions m = mPred
+  mNewData <- data.frame(mReg = mPred)
+  
+  # if nx < ny, set p-value in 0 sub-orbit to 1 and 
+  # estimate p-value in sub-orbit nx
+  if (nx < ny) { 
+      
+    # note: predictions with type="response" are not reliable for small values. There is not a problem for type="link"
+    pPoisCount <- c(B + 1*adjusted,
+      exp(predict(poisFit, newdata=mNewData, type="link")))
 
-		pwTilde <- pPoisCount %*% pmf / (B + 1*adjusted)
-	
-	# if nx==ny, set both the 0 and nx sub-orbit to 1
-	} else { 
-	
-		mNewData <-data.frame(mReg = mNewData$mReg[-nx])
-		
-		pPoisCount <- c(B + 1*adjusted,
-		  exp(predict(poisFit, newdata = mNewData, type="link")),
-			B + 1*adjusted)
-		pwTilde <- pPoisCount %*% pmf / (B + 1*adjusted)
+    pwTilde <- pPoisCount %*% pmf / (B + 1*adjusted)
+  
+  # if nx==ny, set both the 0 and nx sub-orbit to 1
+  } else { 
+  
+    mNewData <-data.frame(mReg = mNewData$mReg[-nx])
+    
+    pPoisCount <- c(B + 1*adjusted,
+      exp(predict(poisFit, newdata = mNewData, type="link")),
+      B + 1*adjusted)
+    
+    pwTilde <- pPoisCount %*% pmf / (B + 1*adjusted)
 
-	}
-	
-	glmSummary <- summary(poisFit)
+  }
+  
+  glmSummary <- summary(poisFit)
 
-	ret <- list(pwTilde = pwTilde,
-			  mStop = m,
-			  deviance = glmSummary$deviance,
-			  aic = glmSummary$aic,
-				df.residual = glmSummary$df.residual,
-				usingStirling = usingStirling,
-				B = B,
-				t0 = t0,
-				comparison = attributes(testStat)$comparison,
-				summary = attributes(testStat)$summary)
-				
-	class(ret) <- "fastPerm"
-	
-	return(ret)
+  ret <- list(pwTilde = pwTilde,
+        mStop = m,
+        deviance = glmSummary$deviance,
+        aic = glmSummary$aic,
+        df.residual = glmSummary$df.residual,
+        usingStirling = usingStirling,
+        B = B,
+        t0 = t0,
+        comparison = attributes(testStat)$comparison,
+        summary = attributes(testStat)$summary)
+        
+  class(ret) <- "fastPerm"
+  
+  return(ret)
 }
 
 print.fastPerm <- function(fp){
-	#' Print function for fastPerm
-	#'
-	#' This function prints the reslts of fastPerm
-	#' @param fp Output from the fastPerm function
-	#' @keywords fastPerm print
-	#' @export
-	#' @examples
-	#' x <- rexp(100, 4)
-	#' y <- rexp(100, 2)
-	#' print(fastPerm(x, y, testStat = ratioMean))
-	
-	result <- paste("    fastPerm Two Sample Test   \n\n",
-			fp$comparison, " of ", fp$summary, "s\n", 
-			prettyNum(fp$B, big.mark = ","), " iterations within partitions",
-			"\n\nobserved statistic = ", signif(fp$t0,3),
-			"\np-value = ", signif(fp$pwTilde,3),
-			"\n\nusing stirling approximation: ", ifelse(fp$usingStirling, "yes", "no"),
-			"\nmStop = ", fp$mStop, ", deviance = ", signif(fp$deviance,3), ", AIC = ", signif(fp$aic,3), sep = "")
-	
-	writeLines(result)
+  #' Print function for fastPerm
+  #'
+  #' This function prints the reslts of fastPerm
+  #' @param fp Output from the fastPerm function
+  #' @keywords fastPerm print
+  #' @export
+  #' @examples
+  #' x <- rexp(100, 4)
+  #' y <- rexp(100, 2)
+  #' print(fastPerm(x, y, testStat = ratioMean))
+  
+  result <- paste("    fastPerm Two Sample Test   \n\n",
+    fp$comparison, " of ", fp$summary, "s\n", 
+    prettyNum(fp$B, big.mark = ","), " iterations within partitions",
+    "\n\nobserved statistic = ", signif(fp$t0,3),
+    "\np-value = ", signif(fp$pwTilde,3),
+    "\n\nusing stirling approximation: ", ifelse(fp$usingStirling, "yes", "no"),
+    "\nmStop = ", fp$mStop, ", deviance = ", signif(fp$deviance,3), ", AIC = ", signif(fp$aic,3), sep = "")
+  
+  writeLines(result)
 }
